ee581b0e492678c89cb200dc6a08c11234042ef6
gwendolinenijssen <gwendolinenssn@gmail.com>
2020-10-25 15:30:11 +0100

    Added discord embed for better layout

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@
 import os
 import random
 
+import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import re
@@ -53,13 +54,13 @@ async def roll(ctx, dice: str, advantage=""):
             rolls.append(random.randrange(1, number_of_sides + 1))
             
             if (advantage == "-a"):
-                roll = max(rolls)
+                adv_roll = max(rolls)
                 advantage_string = "advantage"
             elif (advantage == "-d"):
-                roll = min(rolls)
+                adv_roll = min(rolls)
                 advantage_string = "disadvantage"
                 
-            response = str(ctx.author)+", you rolled with "+advantage_string+": "+str(rolls[0])+" and "+str(rolls[1])+". So your roll is "+str(roll)+"."
+            #response = str(ctx.author)+", you rolled with "+advantage_string+": "+str(rolls[0])+" and "+str(rolls[1])+". So your roll is "+str(roll)+"."
 
         # Normal roll
         else:
@@ -70,17 +71,44 @@ async def roll(ctx, dice: str, advantage=""):
     
             response = str(ctx.author)+", you rolled: "+str(', '.join(rolls))+"."
     
+    
+    # Creates the embed to make the message pretty!
+    embed=discord.Embed(title=f'Rolling {number_of_dice}d{number_of_sides}', description=f'The RollingBot has rolled the dice for you, {ctx.author}.', color=0x509590)
+    embed.set_author(name=ctx.author, url='', icon_url=ctx.author.avatar_url)
+    embed.set_thumbnail(url="https://i.pinimg.com/originals/3f/54/c7/3f54c788ff090c52ad9782f96b78000e.jpg")
+    
+    # Display the roll(s)
+    for i in range(0, len(rolls)):
+        roll_nr = i+1
+        embed.add_field(name=f'Roll {roll_nr}', value=str(rolls[i]), inline=True)
+            
     # Give sum of the rolls if more than one dice was used
     if (number_of_dice > 1):
-        addition = 0
-        for roll in rolls:
-            addition += int(roll)
-        response +=" This adds up to "+str(addition)+"."
+        total = 0
+        for i in range(0, len(rolls)):
+            total += int(rolls[i])
+            
+        #response +=" This adds up to "+str(total)+"."
+        embed.add_field(name="Total", value=str(total), inline=False)
     
     if (advantage == "-a" or advantage == "-d"):
         if (number_of_sides != 20 or number_of_dice != 1):
-            response +="\nAdvantage/disadvantage ignored. (Only for single d20 rolls!)"
-        
-    await ctx.send(response)
+            #response +="\nAdvantage/disadvantage ignored. (Only for single d20 rolls!)"
+            embed.set_footer(text="Advantage/disadvantage ignored. (Only for single d20 rolls!)")
+        else:
+            advantage_title = advantage_string.title()
+            embed.add_field(name=f'{advantage_title} roll:', value=str(adv_roll), inline=False)
+            
+            # Check if natural 1 or 20
+            if (adv_roll == 1):
+                embed.add_field(name="Ouch, a natural 1. That must suck.", value="My bad! :)", inline=True)
+            
+            elif (adv_roll == 20):
+                embed.add_field(name="A natural 20! HELL YEAH!!", value="You're welcome, by the way. :)", inline=True)
+
+            embed.set_footer(text=f'Rolled with {advantage_string}.')
+
+    #await ctx.send(response, embed=embed)
+    await ctx.send(None, embed=embed)
 
 bot.run(TOKEN)
